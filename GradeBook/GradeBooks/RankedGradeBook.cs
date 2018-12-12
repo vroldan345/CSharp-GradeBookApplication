b0a36f5ffbb2e849f3256b0893c1dc44faa3e117
scecret <vroldan345@yahoo.com>
2018-12-12 15:41:58 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[threshold] - 1 <= averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
                 return 'A';
             else if (grades[threshold * 2] - 1 <= averageGrade)
                 return 'B';
